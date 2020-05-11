1d829d7d60fe50ed61848df62345e586fe12af2c
markitan1990 <maar201000@gmail.com>
2020-05-11 19:02:18 +0300

    Изменил вывод на экран в UserServlet

--- src/main/java/servlet/UserServlet.java
+++ src/main/java/servlet/UserServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(value = "/user")
 public class UserServlet extends HttpServlet {
@@ -14,14 +15,14 @@ public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-try {
-    String name = session.getAttribute("role").toString();
-    String lastName = session.getAttribute("lastName").toString();
-    session.setAttribute("message", "Приветствую вас " + name + " " + lastName);
-} catch (Exception e) {
-    System.out.println("Вы не можете перейти на данную страницу");
-}
-
+        try {
+            String name = session.getAttribute("role").toString();
+            String lastName = session.getAttribute("lastName").toString();
+            session.setAttribute("message", "Приветствую вас " + name + " " + lastName);
+        } catch (Exception e) {
+            session.setAttribute("message", "<h1>Вы зашли как аноним. Информация не доступна.</h1>");
+            System.out.println("No information for you");
+        }
         getServletContext().getRequestDispatcher("/user.jsp").forward(req, resp);
     }
 }
